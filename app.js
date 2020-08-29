0d082da27238a51cc7521c70680c284928db2a0f
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2020-08-29 17:13:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const sudokuSolver = require("sudoku-solver-js");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const solver = new sudokuSolver();
+
+// var puzzle =
+//     "001700509573024106800501002700295018009400305652800007465080071000159004908007053";
+// console.log(solver.solve(puzzle));
+
+app.get("/", (req, res) => {
+    let p = req.query.puzzle;
+    res.send(solver.solve(p));
+});
+
+app.listen(port, () => {
+    console.log("Server running at port " + port);
+});
+
+// https://s-solver-api.herokuapp.com/
